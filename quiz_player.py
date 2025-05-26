848e4a989de09d84a1eff0cca7c78c66f1c630f7
Winstar <james.canoy87@gmail.com>
2025-05-26 20:45:25 +0800

    Creating class for question and initialize it with question, choices and answer.

---
+++ quiz_player.py
@@ -0,0 +1,8 @@
+import os
+import random
+
+class Question:
+    def __init__(self, question, choices, answer):
+        self.question = question
+        self.choices = choices
+        self.answer = answer.lower()
