bbd72ce4d103671dedbfc1e6c7c3b6a66856672b
Winstar <james.canoy87@gmail.com>
2025-05-26 21:35:42 +0800

    Creating function ask_question to show question.

--- quiz_player.py
+++ quiz_player.py
@@ -6,3 +6,15 @@ class Question:
         self.question = question
         self.choices = choices
         self.answer = answer.lower()
+
+    def ask_question(self, number):
+        print(f"\nQuestion {number}: {self.question}")
+        for option in sorted(self.choices):
+            print(f"{option}) {self.choices[option]}")
+
+        while True:
+            user_input = input("Your answer (a/b/c/d): ").lower()
+            if user_input in self.choices:
+                return user_input == self.answer
+            else:
+                print("Invalid input. Please enter a, b, c, or d.")
